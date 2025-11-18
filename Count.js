f3a2050e9ff06763b489382b4f7359fa6875f723
ryl <ryl12@aber.ac.uk>
2025-11-18 22:57:17 +0000

    Played about with the CSS,mainly the transform on hover effects

--- Count.js
+++ Count.js
@@ -1,5 +1,6 @@
 let count=0;
 let counter=document.querySelector("#counter");
+counter.textContent=count;
 
 
 function addCounter(){
@@ -10,4 +11,13 @@ function addCounter(){
 function removeCounter(){
     count--;
     counter.textContent=count;
+}
+function resetCounter(){
+    count=0;
+    counter.textContent=count;
+}
+function changecolor(){
+ document.getElementsByTagName("body")[0].style.backgroundColor="black";
+ document.getElementsByTagName("body")[0].style.color= "white";
+
 }
